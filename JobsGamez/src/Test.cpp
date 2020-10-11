f38d0c61471261bf0348d992f16ab78c99dc88d0
jersha <jershafelix@gmail.com>
2020-10-11 08:24:01 +0530

    Project Setup

---
+++ JobsGamez/src/Test.cpp
@@ -0,0 +1,11 @@
+#include "Test.h"
+#include <iostream>
+
+namespace JobsGames {
+
+	void Print()
+	{
+		std::cout << "Welcome to JobsGamez" << std::endl;
+	}
+
+}
---
+++ Sandbox/src/Application.cpp
@@ -0,0 +1,9 @@
+namespace JobsGames {
+
+	__declspec(dllimport) void Print();
+
+}
+
+int main() {
+	JobsGames::Print();
+}
